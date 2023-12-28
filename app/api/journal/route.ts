8041d94718c942ca6648ed6e44a5fc8034b86e47
mohsen-rh214 <mohsen.rh0214@gmail.com>
2023-12-28 18:18:39 +0330

    creating new entry

---
+++ app/api/journal/route.ts
@@ -0,0 +1,15 @@
+import { getUserByClerckId } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+import { NextResponse } from "next/server"
+
+export const POST = async () => {
+    const user = await getUserByClerckId()
+    const entry = await prisma.journalEntry.create({
+        data: {
+            userId: user.id,
+            content: 'Write about your day!'
+        }
+    })
+
+    return NextResponse.json({ data: entry })
+}
--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,10 +1,20 @@
+'use client'
+
+import { createNewEntry } from "@/utils/api"
+import { useRouter } from "next/navigation"
+
 const NewEntryCard = () => {
+    const router = useRouter()
+
+    const handleClick = async () => {
+        const data = await createNewEntry()
+        router.push(`/journal/${data.id}`)
+    }
+
     return (
         <div
-            className="cursor-pointer overflow-hidden rounded-lg bg-white shadow"
-        // onClick={handleClick}
-        >
-            <div className="px-4 py-5 sm:p-6">
+            className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
+            <div className="px-4 py-5 sm:p-6" onClick={handleClick}>
                 <span className="text-3xl">New Entry</span>
             </div>
         </div>
