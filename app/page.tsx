7868d7be8ebc824108d2628d0c7e359ff1e0639b
mohsen-rh214 <mohsen.rh0214@gmail.com>
2023-12-28 15:10:30 +0330

    user authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth()
+  let href = userId ? '/journal' : '/new-user'
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center
     text-white">
@@ -11,8 +14,8 @@ export default function Home() {
           your day, your week, your month, even your year.
         </p>
         <div>
-          <Link href='/journal'>
-          <button className="bg-blue-600 text-xl rounded-lg px-4 py-2">get started</button>
+          <Link href={href}>
+            <button className="bg-blue-600 text-xl rounded-lg px-4 py-2">get started</button>
           </Link>
         </div>
       </div>
