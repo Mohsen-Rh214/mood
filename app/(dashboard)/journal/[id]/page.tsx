33dfb37f6ee5d3a319e133f102de7b49efd13426
mohsen-rh214 <mohsen.rh0214@gmail.com>
2023-12-29 20:15:57 +0330

    analysis column state management

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -21,37 +21,13 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
     const entry = await getEntry(params.id)
-    const { mood, summary, moodColor, textColor, subject, negative } = entry?.analysis
-    const analysisData = [
-        { name: 'Summary', value: summary },
-        { name: 'Subject', value: subject },
-        { name: 'Mood', value: mood },
-        { name: 'Negative', value: negative ? 'True' : 'False' },
-    ]
+    
     return (
-        <div className="h-full w-full grid grid-cols-3">
-            <div className="col-span-2 p-6 bg-zinc-300/20">
-                <Editor entry={entry} />
-            </div>
-            <div className="border-l border-black/10">
-                <div className="px-6 py-10" style={{ backgroundColor: moodColor }}>
-                    <h2 className="text-2xl" style={{ color: textColor }}>Mood Color</h2>
-                </div>
-                <div>
-                    <ul>
-                        {analysisData.map(item => (
-                            <li
-                                className="flex items-center justify-between
-                                px-6 py-4 border-t border-b border-black/10"
-                                key={item.name}
-                            >
-                                <span className="text-lg font-semibold">{item.name}</span>
-                                <span>{item.value}</span>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
+        <div className="h-full w-full">
+
+            <Editor entry={entry} />
+
+
         </div>
     )
 }
--- components/Editor.tsx
+++ components/Editor.tsx
@@ -7,25 +7,59 @@ import { useAutosave } from "react-autosave"
 const Editor = ({ entry }) => {
     const [value, setValue] = useState(entry.content)
     const [isLoading, setIsLoading] = useState(false)
+    const [analysis, setAnalysis] = useState(entry.analysis)
+
+    const { mood, summary, moodColor, textColor, subject, negative } = analysis
+    const analysisData = [
+        { name: 'Summary', value: summary },
+        { name: 'Subject', value: subject },
+        { name: 'Mood', value: mood },
+        { name: 'Negative', value: negative ? 'True' : 'False' },
+    ]
+
     useAutosave({
         data: value,
         onSave: async (_value) => {
             setIsLoading(true)
-            const updated = await updateEntry(entry.id, _value)
+            const data = await updateEntry(entry.id, _value)
+            setAnalysis(data.analysis)
             setIsLoading(false)
         }
     })
+
     return (
-        <div className="w-full h-full">
-            {isLoading &&
-                <div>Auto saving...</div>
-            }
-            <textarea
-                style={{ resize: 'none' }}
-                className="w-full h-full p-8 text-xl outline-none"
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-            />
+        <div className="w-full h-full grid grid-cols-3">
+            <div className="col-span-2 p-8 bg-zinc-300/20">
+                {isLoading &&
+                    <div>Auto saving...</div>
+                }
+                <textarea
+                    style={{ resize: 'none' }}
+                    className="w-full p-8 h-full text-xl outline-none shadow"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                />
+            </div>
+
+            <div className="border-l border-black/10">
+                <div className="px-6 py-10" style={{ backgroundColor: moodColor }}>
+                    <h2 className="text-2xl" style={{ color: textColor }}>Mood Color</h2>
+                </div>
+                <div>
+                    <ul>
+                        {analysisData.map(item => (
+                            <li
+                                className="flex items-center justify-between
+                                px-6 py-4 border-t border-b border-black/10"
+                                key={item.name}
+                            >
+                                <span className="text-lg font-semibold">{item.name}</span>
+                                <span className="text-center">{item.value}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
